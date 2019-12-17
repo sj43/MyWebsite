07410ef23821f4d23a4b2c0ebcac7c8ac5e0354c
Seung Hun Jang <sj43@rice.edu>
2019-12-16 23:30:09 -0600

    .DS_Store banished

--- mypersonalwebsite/src/App.js
+++ mypersonalwebsite/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Seung Hun Jang
+          Seung Hun Jang 
         </p>
         <a
           className="App-link"
