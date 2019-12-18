8449213c4cccee6b5f12363aa55849bb117209d8
Seung Hun Jang <sj43@rice.edu>
2019-12-17 23:43:42 -0600

    setting up foundation for my website

--- mypersonalwebsite/src/App.js
+++ mypersonalwebsite/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/header/header';
+import About from './components/about/about';
+import Resume from './components/resume/resume';
+import Portfolio from './components/portfolio/portfolio';
+import Testimonials from  './components/testimonials/testimonials';
+import ContactUs from './components/contactus/contactus';
+import Footer from './components/footer/footer';
+import resumeData from './components/resume/resumeData';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Seung Hun Jang 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <ContactUs />
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ mypersonalwebsite/src/components/resume/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+    "imagebaseurl":"http://localhost:3000/",
+    "name": "Seung Hun Jang",
+    "role": "Computer Science Student @ Rice University"
+    }
+  export default resumeData
