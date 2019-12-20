3b36d9cdd0c2b62706dfa0bb0e30cb19205a161e
Seung Hun Jang <sj43@rice.edu>
2019-12-19 21:56:40 -0600

    fill in WORK section in RESUME section

--- mypersonalwebsite/src/components/resume/resumeData.js
+++ mypersonalwebsite/src/components/resume/resumeData.js
@@ -1,17 +1,54 @@
 let resumeData = {
-    "imagebaseurl":"http://localhost:3000/",
-    "name": "Seung Hun Jang",
-    "role": "Junior at Rice University studying Computer Science",
-    "phone": "(713)806-8991",
-    "address": "Houston, TX",
-    "email": "sj43@rice.edu",
-    "education": {
-      "school": "Rice University",
-      "major": "B.S. in Computer Science",
-      "year": "Junior",
-      "date": "Expected May 2021",
-      "gpa": "3.75/4.0"
+  "imagebaseurl": "http://localhost:3000/",
+  "name": "Seung Hun Jang",
+  "role": "Junior at Rice University studying Computer Science",
+  "phone": "(713)806-8991",
+  "address": "Houston, TX",
+  "email": "sj43@rice.edu",
+  "education": {
+    "school": "Rice University",
+    "major": "B.S. in Computer Science",
+    "year": "Junior",
+    "date": "Expected May 2021",
+    "gpa": "3.75/4.0"
+  },
+  "experience": [
+    {
+      "organization": "V-Silicon Inc.",
+      "location": "Fremont, California",
+      "position": "Software Engineering Intern",
+      "date": "May 2019 - Aug 2019",
+      "description": [
+        "Built video casting applications for internal smart-TV testing (web / Android / iOS)",
+        "Regularly built new patches on Ubuntu and deployed on Jenkins Server",
+        "Fixed potential bugs on Platform Abstraction Layer (PAL) using Coverity static analysis tool, Gerrit Code Review, and JIRA issue tracking software",
+        "Assisted SW and QE teams in demo setup, issue duplication, documentation, etc"
+      ]     
     },
-    "resumeLink": "https://drive.google.com/open?id=14D7ZbdZ4QoRgwBKtYUZfiswGbZyu5-6D"
+    {
+      "organization": "Migeum Computer and Robotics Learning Center",
+      "location": "Yongin, South Korea",
+      "position": "Programming Language Teaching Assistant",
+      "date": "Apr 2018 - Aug 2018",
+      "description": [
+        "Prepared and delivered lectures to learning center students on topics such as syntax, container usage, object-oriented design, problem solving skills, and data structures",
+        "Assisted in developing prototype of center’s student management tools",
+      ]     
+    },
+    {
+      "organization": "U.S. Eighth Army | KATUSA",
+      "location": "Camp Humphreys, South Korea",
+      "position": "Head of Battalion Command Group Admin",
+      "date": "Sep 2016 - Apr 2018",
+      "description": [
+        "Served in the Eighth Army, Headquarters, and Headquarters Battalion Command Group as KATUSA (Korean Augmentation to the United States Army)",
+        "As the team leader, assisted Battalion Commander and Command Sergeant Major in missions",
+        "Supervised Soldiers’ administrative action management process",
+        "Supported and facilitated move of 8,000+ Soldiers from Camp Yongsan to Camp Humphreys",
+        "Resolved technical issues such as ARMY GEARS system VPN, DoD CAC issuance, etc"
+      ]     
     }
-  export default resumeData
+  ],
+  "resumeLink": "https://drive.google.com/open?id=14D7ZbdZ4QoRgwBKtYUZfiswGbZyu5-6D"
+}
+export default resumeData
