ed84a19bfbf5d0b3d5a9a530223fbea96f8f4a0e
Seung Hun Jang <sj43@rice.edu>
2020-03-22 17:49:52 -0500

    Updates

---
+++ precache-manifest.991033cef4c58d9365f9c4e891c0cce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1481c82959b4cc989757a62e749191b2",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "2d0412357887183438b8",
+    "url": "/MyWebsite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d78136b26cf45bb374c7",
+    "url": "/MyWebsite/static/js/2.f4e64848.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyWebsite/static/js/2.f4e64848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0412357887183438b8",
+    "url": "/MyWebsite/static/js/main.38af567b.chunk.js"
+  },
+  {
+    "revision": "7728289db7cecd3bbe38",
+    "url": "/MyWebsite/static/js/runtime-main.59d43feb.js"
+  }
+]);
