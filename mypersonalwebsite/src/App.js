9f5251248df86c9698d9c7e10fc219ea3da82b0e
Seung Hun Jang <sj43@rice.edu>
2020-03-22 21:15:46 -0500

    temporarily hid portfolio aspect

--- mypersonalwebsite/src/App.js
+++ mypersonalwebsite/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/header/header';
 import About from './components/about/about';
 import Resume from './components/resume/resume';
-import Portfolio from './components/portfolio/portfolio';
+// import Portfolio from './components/portfolio/portfolio';
 // import Testimonials from  './components/testimonials/testimonials';
 // import ContactUs from './components/contactus/contactus';
 import Footer from './components/footer/footer';
@@ -16,7 +16,7 @@ class App extends Component {
         <Header resumeData={resumeData}/>
         <About resumeData={resumeData}/>
         <Resume resumeData={resumeData} projectData={projectData}/>
-        <Portfolio />
+        {/* <Portfolio /> */}
         {/* <Testimonials /> */}
         {/* <ContactUs /> */}
         <Footer />
